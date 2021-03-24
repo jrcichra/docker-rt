34684b8e19f271460bb93a2008403eb60b2f250b
Justin Cichra <1342149+jrcichra@users.noreply.github.com>
2021-03-23 22:49:29 -0400

    RT_WEB_DOMAIN

--- RT_SiteConfig.pm
+++ RT_SiteConfig.pm
@@ -1,6 +1,7 @@
 use utf8;
 
-Set($WebPort, RT_WEB_PORT);
-Set($MailCommand, "testfile");
+Set( $WebPort,     RT_WEB_PORT );
+Set( $MailCommand, "testfile" );
+Set( $WebDomain,   'RT_WEB_DOMAIN' );
 
 1;
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,9 @@
 set -euo pipefail
 
 : "${RT_WEB_PORT:=80}"
+: "${RT_WEB_DOMAIN:=localhost}"
 
 sed -i "s/RT_WEB_PORT/$RT_WEB_PORT/" /opt/rt%%RT_VERSION_MAJOR%%/etc/RT_SiteConfig.pm
+sed -i "s/RT_WEB_DOMAIN/$RT_WEB_DOMAIN/" /opt/rt%%RT_VERSION_MAJOR%%/etc/RT_SiteConfig.pm
 
 exec "$@"
