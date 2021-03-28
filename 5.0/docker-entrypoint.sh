d9012bff073dde8da7ef082f0bb10fab7640b681
Justin Cichra <1342149+jrcichra@users.noreply.github.com>
2021-03-28 12:29:54 -0400

    sigh, copy and paste

--- 5.0/docker-entrypoint.sh
+++ 5.0/docker-entrypoint.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 
 : "${RT_WEB_PORT:=80}"
 
-sed -i "s/RT_WEB_PORT/$RT_WEB_PORT/" /opt/rt5/etc/RT_SiteConfig.pm
+sed -i "s/RT_WEB_PORT/$RT_WEB_PORT/g" /opt/rt5/etc/RT_SiteConfig.pm
+sed -i "s/RT_WEB_DOMAIN/$RT_WEB_DOMAIN/g" /opt/rt5/etc/RT_SiteConfig.pm
 
 exec "$@"
