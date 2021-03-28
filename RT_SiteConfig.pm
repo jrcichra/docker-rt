bfc5497d3ac15256abcbabe98ccf2f3012571606
Justin Cichra <1342149+jrcichra@users.noreply.github.com>
2021-03-28 12:16:35 -0400

    add whitelist for domain and port

--- RT_SiteConfig.pm
+++ RT_SiteConfig.pm
@@ -3,5 +3,7 @@ use utf8;
 Set( $WebPort,     RT_WEB_PORT );
 Set( $MailCommand, "testfile" );
 Set( $WebDomain,   'RT_WEB_DOMAIN' );
+Set( @ReferrerWhitelist,
+    qw(RT_WEB_DOMAIN:RT_WEB_PORT localhost:80 127.0.0.1:80) );
 
 1;
